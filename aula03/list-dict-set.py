cf44929de5fc2ebac026d73ece6a96650026ba4d
YuriAoyamaSE <aoyamabrasil@gmail.com>
2022-02-22 21:44:12 -0300

    aula03

---
+++ aula03/list-dict-set.py
@@ -0,0 +1,21 @@
+"""
+Bootcamp código[s]
+Data: 22/02/2022
+aula 03 
+
+lista = [... , ...] 
+tupla = (... , ...)
+dicionario = {chave : valor , ...}
+"""
+
+dicionario = {"SE":"Aracaju", "SP":"São Paulo"}
+
+print(dicionario)
+
+dicionario["BA"] = "Salvador"
+
+print(dicionario)
+
+print(dicionario.items())
+print(dicionario.keys())
+print(dicionario.values())
