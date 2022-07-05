dace317cd3c2338fd29754467ccd5687ddecf264
Yuri <aoyamabrasil@gmail.com>
2022-07-05 20:06:01 -0300

    base

--- modulo_2/django_rest_2/configuracoes/urls.py
+++ modulo_2/django_rest_2/configuracoes/urls.py
@@ -1,4 +1,4 @@
-"""setup URL Configuration
+"""configuracoes URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,16 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
-from aluguel_veiculos.views import ClienteViewSet, VeiculoViewSet
-
-router = routers.DefaultRouter()
-router.register(r'cliente', ClienteViewSet)
-router.register(r'veiculo', VeiculoViewSet)
+from django.urls import path
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
 ]
